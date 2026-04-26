920a506075557135f4c34befaf5606a0ca7c2f01
nhmokhocjc025 <nhmokhocjc025@student.wethinkcode.co.za>
2026-04-27 00:31:24 +0200

    added code transform method.

--- etl/etl.py
+++ etl/etl.py
@@ -1,14 +1,26 @@
-import pandas as pd 
+import pandas as pd
+import datetime as dt
 
 def extract(file_path:str) -> pd.DataFrame:
     #Extarcting raw data from our flatfile
     print(f"\nExtracting data from {file_path}...\n")
     return pd.read_csv(file_path)
 
-print(extract("data/sales.csv").head())
+#print(extract("data/sales.csv").head())
 
-def transform():
-    pass
+def transform(df_raw:pd.DataFrame)-> pd.DataFrame:
+    #calculate the total using quantity * price
+    df_raw['total_amount'] = df_raw['quantity'] * df_raw['price']
+    #convert date column in order_data using datetime method
+    df_raw['order_date'] = pd.to_datetime(df_raw['order_date'])
+    #standardise every region to uppercase
+    df_raw['region'] = df_raw["region"].str.upper()
+    #drop duplicates using drop_duplicates() method
+    df_raw = df_raw.drop_duplicates(subset=["order_id"])
+
+    return df_raw
+
+print(transform(extract("data/sales.csv")))
 
 def load():
     pass
